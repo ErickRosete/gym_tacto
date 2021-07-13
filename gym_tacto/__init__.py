d58e6f403df2d92803680591b87549ed0c205b90
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2021-07-13 14:22:17 +0200

    Added max episode steps in registration

--- gym_tacto/__init__.py
+++ gym_tacto/__init__.py
@@ -3,9 +3,11 @@ from gym.envs.registration import register
 register(
     id='sawyer-peg-v0',
     entry_point='gym_tacto.envs:SawyerPegV0',
+    max_episode_steps=500
 )
 
 register(
     id='sawyer-peg-v1',
     entry_point='gym_tacto.envs:SawyerPegV1',
+    max_episode_steps=500
 )
